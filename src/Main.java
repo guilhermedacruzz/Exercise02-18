1490b17eb568c8bfdadaf92b1c52356d33e0ccff
Guilherme da Cruz <gddcruz04@gmail.com>
2020-02-18 14:48:35 -0300

    Exercício da Mega Sena

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        int aposta[] = new int[6];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 6; i++) {
+            System.out.printf("Digite o número %d:\t", i + 1);
+            aposta[i] = sc.nextInt();
+
+            if(aposta[i] >= 1 && aposta[i] <= 60) {
+                for(int f = 0; f < i; f++) {
+                    if(aposta[f] == aposta[i]) {
+                        System.out.println("Número Repitido!");
+                        new BufferedReader(new InputStreamReader(System.in)).readLine();
+                        i--;
+                    }
+                }
+            }
+            else{
+                System.out.println("Número Digitado fora dos Parâmetros!");
+                new BufferedReader(new InputStreamReader(System.in)).readLine();
+                i--;
+            }
+        }
+
+        MegaSena mg = new MegaSena();
+        mg.verificarVencedor(aposta);
+    }
+}
---
+++ src/MegaSena.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+public class MegaSena {
+    private int[] sorteio = new int[6];
+
+    public MegaSena(){
+        for(int i = 0; i < 6; i++){
+            this.sorteio[i] = new Random().nextInt() % 60 + 1;
+        }
+    }
+
+    public void verificarVencedor(int aposta[]) {
+        int acertos = 0;
+
+        for(int i = 0; i < 6; i++)
+            for(int f = 0; f < 6; f++)
+                if(this.sorteio[f] == aposta[i])
+                    acertos++;
+
+        this.imprimirResultado(acertos);
+    }
+
+    private void imprimirResultado(int acertos){
+        switch(acertos){
+            case 1:
+                System.out.printf("Putz! Não foi dessa fez.....Você acertou apenas um número...\n");
+                break;
+
+            case 2:
+                System.out.printf("Parabéns! Você acertou dois números! Mas não ganhou nada\n");
+                break;
+
+            case 3:
+                System.out.printf("Parabéns! Você acertou metade!\n");
+                break;
+
+            case 4:
+                System.out.printf("Parabéns! Você acertou uma Quadra! Ganhou 400 reais!\n");
+                break;
+
+            case 5:
+                System.out.printf("Parabéns! Você acertou uma Quinta! Ganhou 1500 reais!\n");
+                break;
+
+            case 6:
+                System.out.printf("Parabéns! Você está milionário!\n");
+                break;
+
+            default:
+                System.out.printf("Que pena! Você não acertou nada.... Continue tentando.....\n");
+                break;
+        }
+    }
+}
